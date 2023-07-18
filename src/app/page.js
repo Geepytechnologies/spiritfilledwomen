a6f044e60e5546ea87f797832bfd6b42bb0f1acc
geepy technologies <geepytechnologies@gmail.com>
2023-07-18 18:21:56 +0100

    fresh

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Header from "../../components/Header";
+
+import Header from "../components/Header";
 
 export default function Home() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1 className='text-[20px]'>Spirit filled women international</h1>
+            <h1 className='text-[20px] font-[500]'>Spirit filled women international</h1>
         </div>
     )
 }
